f3a20bc1b4a006877461565831b933e81ff173b6
Konrad Duda <konrad.duda121@gmail.com>
2023-07-26 14:15:24 +0200

    Add new api resources

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CompanyController;
+use App\Http\Controllers\Api\V1\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'as' => 'api.'], function(){
+    Route::apiResource('employees', EmployeeController::class);
+    Route::apiResource('companies', CompanyController::class);
 });
+
