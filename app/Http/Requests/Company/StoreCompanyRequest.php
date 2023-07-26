cdcb030c7f4374e29a67df742d6fedd7eaa2a4df
Konrad Duda <konrad.duda121@gmail.com>
2023-07-26 16:14:29 +0200

    Basic Company form request

---
+++ app/Http/Requests/Company/StoreCompanyRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Company;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:32'],
+            'nip' => ['required', 'string', 'min:10', 'max:10'],
+            'address' => ['required', 'string', 'min:3', 'max:128'],
+            'city' => ['required', 'string', 'min:3', 'max:64'],
+            'zipcode' => ['required', 'string', 'min:3', 'max:16'],
+        ];
+    }
+}
